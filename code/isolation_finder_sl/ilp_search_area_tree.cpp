6730fd1672c036e6e76d14b375f49af5009e1e71
Nicolai Huening <nicolai-huening@gmx.de>
2023-04-22 06:53:17 +0200

    unbound sweepline to new format

--- code/isolation_finder_sl/ilp_search_area_tree.cpp
+++ code/isolation_finder_sl/ilp_search_area_tree.cpp
@@ -1,7 +1,8 @@
 #include "ilp_search_area_tree.h"
-#include "tile_cell.h"
 #include "sweepline_datastruct.h"
 #include "sweepline_datastruct_quadtree_static.h"
+#include "sweepline_primitives.h"
+#include "tile_cell.h"
 
 ILPSearchAreaTree::ILPSearchAreaTree(int minLat, int minLng, int offsetLat,
                                      int offsetLng) {
@@ -15,11 +16,9 @@ ILPSearchAreaTree::ILPSearchAreaTree(int minLat, int minLng, int offsetLat,
 ILPSearchAreaTree::~ILPSearchAreaTree() { delete mRoot; }
 
 void ILPSearchAreaTree::distributeToTiles(const LatLng &peakLocation,
-                                          int elevation,
-                                          float *isolationKm) {
+                                          int elevation, float *isolationKm) {
   if (*isolationKm > 0) {
-    mRoot->distributeToTiles(peakLocation, elevation,
-                               *isolationKm);
+    mRoot->distributeToTiles(peakLocation, elevation, *isolationKm);
   } else {
     // Add first to unbound and proccess at the end.
     mUnboundResults.addResult(peakLocation, elevation, *isolationKm);
@@ -33,15 +32,19 @@ ConcurrentIsolationResults *ILPSearchAreaTree::findBucket(int minLat,
 
 void ILPSearchAreaTree::registerTile(int minLat, int minLng,
                                      Elevation maxElev) {
-  mTileEvents.push_back(SlEvent(maxElev, false, LatLng(static_cast<float>(minLat), static_cast<float>(minLng))));
+  mTileEvents.push_back(
+      SlEvent(maxElev, SlEventType::ADD,
+              LatLng(static_cast<float>(minLat), static_cast<float>(minLng))));
   mRoot->registerTile(minLat, minLng, maxElev);
 }
 
 void ILPSearchAreaTree::proccessUnbound() {
-  SlEvent *queue = new SlEvent[mUnboundResults.mResults.size() + mTileEvents.size()];
+  SlEvent *queue =
+      new SlEvent[mUnboundResults.mResults.size() + mTileEvents.size()];
   int idx = 0;
   for (auto &res : mUnboundResults.mResults) {
-    queue[idx].initialize(res.peakElevation, true, res.peak, Offsets());
+    queue[idx].initialize(res.peakElevation, SlEventType::PEAK, res.peak,
+                          Offsets());
     idx++;
   }
   for (auto &tile : mTileEvents) {
@@ -49,28 +52,40 @@ void ILPSearchAreaTree::proccessUnbound() {
     idx++;
   }
   std::stable_sort(queue, queue + idx,
-                     [](SlEvent const &lhs, SlEvent const &rhs) {
+                   [](SlEvent const &lhs, SlEvent const &rhs) {
                      return lhs.getElev() > rhs.getElev();
                    });
   SweeplineDatastruct *sld = new SweeplineDatastructQuadtreeStatic<16>(
-          mMinLat, mMaxLat, mMinLng, mMaxLng,
-          (mMaxLat - mMinLat) * (mMaxLng - mMinLng),
-          [=](float lat, float lng) { return Offsets(); });
+      mMinLat, mMaxLat, mMinLng, mMaxLng,
+      (mMaxLat - mMinLat) * (mMaxLng - mMinLng),
+      [=](float lat, float lng) { return Offsets(); });
   // run sweepline
   for (int i = 0; i < idx; i++) {
-    if (queue[i].isPeak()) {
+    switch (queue[i].getType()) {
+    case PEAK: {
       IsolationRecord nearestTile = sld->calcPeak(queue + i);
       if (!nearestTile.foundHigherGround) {
-        //std::cout << "Did not find higher ground for: " << queue[i].latitude() << ", " << queue[i].longitude() << std::endl;
+        // std::cout << "Did not find higher ground for: " <<
+        // queue[i].latitude() << ", " << queue[i].longitude() << std::endl;
         continue;
       }
-      LatLng topLeft(std::floor(nearestTile.closestHigherGround.latitude()) + 1, 
-      std::floor(nearestTile.closestHigherGround.longitude()) + 1);
-      LatLng bottomRight(std::floor(nearestTile.closestHigherGround.latitude()), 
-      std::floor(nearestTile.closestHigherGround.longitude()));
-      mRoot->distributeToTiles(queue[i], queue[i].getElev(), (nearestTile.distance + searchDistance(&topLeft, bottomRight)) / 1000);
-    } else {
+      LatLng topLeft(std::floor(nearestTile.closestHigherGround.latitude()) + 1,
+                     std::floor(nearestTile.closestHigherGround.longitude()) +
+                         1);
+      LatLng bottomRight(
+          std::floor(nearestTile.closestHigherGround.latitude()),
+          std::floor(nearestTile.closestHigherGround.longitude()));
+      mRoot->distributeToTiles(
+          queue[i], queue[i].getElev(),
+          (nearestTile.distance + searchDistance(&topLeft, bottomRight)) /
+              1000);
+    } break;
+    case ADD:
       sld->insert(queue + i);
+      break;
+    case REMOVE:
+      sld->remove(queue + i);
+      break;
     }
   }
 }
