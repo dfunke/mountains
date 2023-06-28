df87bfb8d5cfaa5c7c598d69a5d0c671cdcc3e2e
Nicolai Huening <nicolai-huening@gmx.de>
2023-06-28 09:13:22 +0200

    Changed to moon

--- code/speedtesting.cpp
+++ code/speedtesting.cpp
@@ -37,7 +37,7 @@ using std::string;
 static const string baseFolder = "/data02/funke/SRTM/viewfinderpanoramas.org/dem3"; 
 static const string baseFolderDem1 = "/data02/funke/SRTM/viewfinderpanoramas.org/dem1"; 
 static const string testFolder = "/data02/funke/SRTM/Moon/dem15"; 
-static const string testResultFile = "/home/huening/testresults-world-dem3-randomsample.txt";
+static const string testResultFile = "/home/huening/testresults-moon.txt";
 
 //static const string baseFolder = "/home/pc/Data1/Mars/dem15";
 //static const string baseFolderDem1 = "/home/pc/SRTM-DEM1";
